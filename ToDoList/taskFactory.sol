92bc7d6136a6709fb6842b17407bbe086d72fd3a
asharl <i.am.ayush.agrawal.123@gmail.com>
2026-01-16 20:34:48 +0530

    bug fixes and code organisation

--- ToDoList/taskFactory.sol
+++ ToDoList/taskFactory.sol
@@ -1,59 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^ 0.8.30;
 
-import "@openzeppelin/contracts/utils/Strings.sol";
-
 contract TodoList {
 
   //Priority level  for the user's tasks 
   // Enter  0 for Low, 1 for Medium, 2 for High priorities
-  enum Priority { Low, Medium, High } address public owner;
+  enum Priority { Low, Medium, High }
 
   //Struct for tasks
   struct Task {
     uint id;
     string taskDescription;
     string taskCategory;
+    string taskDueDate;
     Priority taskPriority;
-    uint64 taskDeadline;
     bool taskStatus;
   }
 
+  //user based mapping 
   mapping(address => Task[]) private userTasks;
   mapping(address => string[]) private userCategories;
-  mapping(address => uint[]) private taksCounter;
-  
-  constructor() { owner = msg.sender; }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner, "Not authorized");
-    _;
-  }
+  mapping(address => uint) private taskCounter;
 
   modifier checkRange(uint taskId) {
-    require(taskId < taskList.length, "Invalid Task");
+    require(taskId < userTasks[msg.sender].length, "Invalid Task");
     _;
   }
 
-  
-
-  Task[] internal taskList;
-  string[] internal categories;
-
-  function newTask(string memory taskDescription, string memory taskCategory,
-                   Priority taskPriority, uint64 taskDeadline) public onlyOwner(){
-
-    listCategories(taskCategory); 
-
-    string memory taskDesc = string(abi.encodePacked(
-        Strings.toString(taskList.length), "-", taskDescription));
-
-    taskList.push(
-        Task(taskDesc, taskCategory, taskPriority, taskDeadline, false));
-  }
-
-// Keeps a track of the cateogries the user has created for easy accessibility.
-  function listCategories(string memory taskCategory) internal onlyOwner(){
+  //function that will store all the categories for future use
+  function listCategories(address user, string memory taskCategory) internal{
+    string[] storage categories = userCategories[user];
     for (uint i = 0; i < categories.length; i++) {
       if (keccak256(bytes(categories[i])) == keccak256(bytes(taskCategory))) {
         return;
@@ -62,56 +38,79 @@ contract TodoList {
     categories.push(taskCategory);
   }
 
-//lists the tasks
-  function listTask() public view returns(string[] memory) {
-    string[] memory tasks = new string[](taskList.length);
+  //function to add new task
+  function newTask(string memory taskDescription, string memory taskCategory, string memory taskDueDate,
+                   Priority taskPriority) public{
 
-    for (uint i = 0; i < taskList.length; i++) {
-      tasks[i] = taskList[i].taskDescription;
-    }
+    listCategories(msg.sender, taskCategory); 
 
-    return tasks;
+    userTasks[msg.sender].push(Task({id : taskCounter[msg.sender], taskDescription : taskDescription, taskCategory :
+    taskCategory, taskDueDate : taskDueDate, taskPriority : taskPriority, taskStatus : false}));
+
+    taskCounter[msg.sender]++;
   }
 
-// to remove a task
-  function removeTask(uint taskId) public checkRange(taskId) onlyOwner(){
-    uint last = taskList.length - 1;
+  //this function which will change the status of the task 
+  function taskStatusChange(uint taskId) public checkRange(taskId) returns(Task memory) {
+    userTasks[msg.sender][taskId].taskStatus = !userTasks[msg.sender][taskId].taskStatus;
+
+    return userTasks[msg.sender][taskId];
+  }
+
+  //this function lets user change the description of their task by selecting a task ID
+  function editTaskDescription(uint taskId, string memory newDescription) public checkRange(taskId){
+    userTasks[msg.sender][taskId].taskDescription = newDescription;
+}
+
+  //this function lets a user to remove a task by the task ID
+  function removeTask(uint taskId) public checkRange(taskId) {
+    uint last = userTasks[msg.sender].length - 1;
     if (last != taskId) {
-      taskList[taskId] = taskList[last];
+      userTasks[msg.sender][taskId] = userTasks[msg.sender][last];
+      userTasks[msg.sender][taskId].id = taskId;
     }
-    taskList.pop();
+    userTasks[msg.sender].pop();
   }
 
-  // lists the task by their category
-  function ListByCategory(string memory category) public view returns(Task[] memory) {
-    uint count = 0;
-
-    for (uint i = 0; i < taskList.length; i++) {
-      if (keccak256(bytes(category)) ==
-          keccak256(bytes(taskList[i].taskCategory))) {
-        count++;
-      }
-    }
-
-    Task[] memory tasks = new Task[](count);
-    uint j = 0;
-
-    for (uint i = 0; i < taskList.length; i++) {
-      if (keccak256(bytes(category)) ==
-          keccak256(bytes(taskList[i].taskCategory))) {
-        tasks[j++] = taskList[i];
-      }
-    }
-
-    return tasks;
+  //lists the tasks 
+  function listTask() public view returns(Task[] memory) {
+    return userTasks[msg.sender];
+  }
+  
+  
+  function callTask(uint taskId) public view checkRange(taskId) returns(Task memory) {
+    return userTasks[msg.sender][taskId];
   }
 
   // lists the task by their status
   function ListByStatus(bool status) public view returns(Task[] memory) {
     uint count = 0;
 
-    for (uint i = 0; i < taskList.length; i++) {
-      if (status == taskList[i].taskStatus) {
+    for (uint i = 0; i < userTasks[msg.sender].length; i++) {
+      if (status == userTasks[msg.sender][i].taskStatus) {
+        count++;
+      }
+    }
+
+    Task[] memory filteredTasks = new Task[](count);
+    uint j = 0;
+
+    for (uint i = 0; i < userTasks[msg.sender].length; i++) {
+      if (status == userTasks[msg.sender][i].taskStatus) {
+        filteredTasks[j++] = userTasks[msg.sender][i];
+      }
+    }
+
+    return filteredTasks;
+  }
+
+  // lists the task by their category
+  function ListByCategory(string memory category) public view returns(Task[] memory) {
+    uint count = 0;
+
+    for (uint i = 0; i < userTasks[msg.sender].length; i++) {
+      if (keccak256(bytes(category)) ==
+          keccak256(bytes(userTasks[msg.sender][i].taskCategory))) {
         count++;
       }
     }
@@ -119,21 +118,24 @@ contract TodoList {
     Task[] memory tasks = new Task[](count);
     uint j = 0;
 
-    for (uint i = 0; i < taskList.length; i++) {
-      if (status == taskList[i].taskStatus) {
-        tasks[j++] = taskList[i];
+    for (uint i = 0; i < userTasks[msg.sender].length; i++) {
+      if (keccak256(bytes(category)) ==
+          keccak256(bytes(userTasks[msg.sender][i].taskCategory))) {
+        tasks[j++] = userTasks[msg.sender][i];
       }
     }
 
     return tasks;
   }
 
+  
+
   function listByPriority() public view returns (Task[] memory) {
-    Task[] memory tasks = new Task[](taskList.length);
+    Task[] memory tasks = new Task[](userTasks[msg.sender].length);
 
     // Copy storage to  memory in oredr to save gas
-    for (uint i = 0; i < taskList.length; i++) {
-        tasks[i] = taskList[i];
+    for (uint i = 0; i < userTasks[msg.sender].length; i++) {
+        tasks[i] = userTasks[msg.sender][i];
     }
 
     if (tasks.length == 0) return tasks;
@@ -162,22 +164,8 @@ contract TodoList {
 }
 
 
-  function taskStatusChange(uint taskId) public checkRange(taskId) onlyOwner() returns(Task memory) {
-    taskList[taskId].taskStatus = !taskList[taskId].taskStatus;
-
-    return taskList[taskId];
-  }
-
-  function callTask(uint taskId) public view checkRange(taskId) returns(Task memory) {
-    return taskList[taskId];
-  }
-
-  function editTaskDescription(uint taskId, string memory newDescription) public checkRange(taskId) onlyOwner(){
-    taskList[taskId].taskDescription = newDescription;
-}
-
 function getCategories() public view returns (string[] memory) {
-    return categories;
+    return userCategories[msg.sender];
 }
 
 
